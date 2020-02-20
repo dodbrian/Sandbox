b3426242db290522f61605bb781a1e00e9333705
Denis Zimin <denis.zimin@complevo.de>
2020-02-20 13:20:26 +0100

    add Common project

---
+++ Sandbox.Common/ColoredConsole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sandbox.Common
+{
+    public static class ColoredConsole
+    {
+        public static void WriteLine(string text, ConsoleColor color)
+        {
+            var currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(text);
+
+            Console.ForegroundColor = currentColor;
+        }
+
+        public static async Task WriteLineAsync(string text, ConsoleColor color)
+        {
+            var currentColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+
+            await Console.Out.WriteLineAsync(text);
+
+            Console.ForegroundColor = currentColor;
+        }
+    }
+}
