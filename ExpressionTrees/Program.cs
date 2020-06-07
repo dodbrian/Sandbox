e2172160c530e32424c3be853d1e6de51995ae6d
Denis Zimin <roydronov@gmail.com>
2020-06-07 23:13:42 +0200

    test LinqKit Expand

--- ExpressionTrees/Program.cs
+++ ExpressionTrees/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
+using LinqKit;
 
 namespace ExpressionTrees
 {
     internal class Program
     {
         private static void Main(string[] args)
+        {
+            Expression<Func<SampleClass, bool>> criteria1 = p => p.NumericProp > 10;
+            Expression<Func<SampleClass, bool>> criteria2 = p => criteria1.Invoke(p) || p.StringProp.Contains("test");
+
+            Console.WriteLine(criteria1.Expand().ToString());
+            Console.WriteLine(criteria2.Expand().ToString());
+        }
+
+        private static void InfoDemo()
         {
             var absMethod = Utils.InfoOf(() => Math.Abs(default(double)));
             var nowProperty = Utils.InfoOf(() => DateTime.Now);
@@ -18,8 +28,8 @@ namespace ExpressionTrees
             var table = new Source<int>("numbers");
 
             var result = from x in table
-                where x > 0
-                select x * x;
+                         where x > 0
+                         select x * x;
 
             foreach (var x in result) Console.WriteLine(x);
         }
---
+++ ExpressionTrees/SampleClass.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ExpressionTrees
+{
+    public class SampleClass
+    {
+        public int NumericProp { get; set; }
+
+        public string StringProp { get; set; }
+
+        public Guid GuidProp { get; set; }
+    }
+}
