6e801064d890317bd2433f63963fe69d0d298cf4
Denis Zimin <roydronov@gmail.com>
2024-04-24 20:16:23 +0200

    add Kafka consumer

--- Kafka/KafkaConsumer/Program.cs
+++ Kafka/KafkaConsumer/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Confluent.Kafka;
 
-Console.WriteLine("Hello, World!");
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092,localhost:9093,localhost:9094",
+    GroupId = "consumer",
+};
+
+using var consumer = new ConsumerBuilder<string, double>(consumerConfig).Build();
+consumer.Subscribe("myorders");
+
+while (true)
+{
+    var consumeResult = consumer.Consume();
+    Console.WriteLine($"Consumed message with key {consumeResult.Message.Key} and value {consumeResult.Message.Value}");
+}
